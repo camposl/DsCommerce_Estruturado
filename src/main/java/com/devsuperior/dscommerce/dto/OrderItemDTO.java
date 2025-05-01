d9d5461a96c140ab7aa19607a1f12336845f216a
camposl <lucascampos25@gmail.com>
2025-05-01 01:52:20 -0300

    OrderDTO, get order by id

---
+++ src/main/java/com/devsuperior/dscommerce/dto/OrderItemDTO.java
@@ -0,0 +1,45 @@
+package com.devsuperior.dscommerce.dto;
+
+import com.devsuperior.dscommerce.entities.OrderItem;
+
+public class OrderItemDTO {
+
+    private Long productId;
+    private String name;
+    private Double price;
+    private Integer quantity;
+
+    public OrderItemDTO(Long productId, String name, Double price, Integer quantity) {
+        this.productId = productId;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public OrderItemDTO(OrderItem enity) {
+        productId = enity.getProduct().getId();
+        name = enity.getProduct().getName();
+        price = enity.getPrice();
+        quantity = enity.getQuantity();
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public Double getSubTotal(){
+        return price * quantity;
+    }
+}
